e3f57b37a39215af3b9bf4f401cbd0cc8576bf65
PC55 <laryea.enl99@gmail.com>
2022-03-11 14:57:31 +0000

    update

--- assets/customjs/main.js
+++ assets/customjs/main.js
@@ -13,6 +13,7 @@ eventListeners()
 function eventListeners() {
     window.addEventListener('DOMContentLoaded', () => {
         loadJSON();
+        loadCart();
     })
     // navbar toggler
     navbarToggler.addEventListener('click', () => {
@@ -24,10 +25,20 @@ function eventListeners() {
         cartContainer.classList.toggle('show-cart-container')
     })
 
-    document.querySelector('.cart-item-del-btn').addEventListener('click', () => {
-        cartContainer.classList.toggle('show-cart-container')
-    }) 
+    // document.querySelector('.cart-item-del-btn').addEventListener('click', () => {
+    //     cartContainer.classList.toggle('show-cart-container')
+    // })
+
+    productList.addEventListener('click', purchaseProduct)
 }
+
+// update cart info
+function updateCartInfo() {
+    let cartInfo = findCartInfo();
+   
+}
+
+updateCartInfo()
 
 function loadJSON() {
     fetch('assets/building.json')
@@ -38,7 +49,7 @@ function loadJSON() {
                 html += `
                 <div class="product-item">
                     <div class="product-img">
-                        <img src="${product.imgSrc}" alt="product image">
+                        <img class='img' src="${product.imgSrc}" alt="product image">
                         <button type="button" class="add-to-cart-btn">
                             <i class="fas fa-shopping-cart"> </i>
                             Add To Cart
@@ -47,9 +58,7 @@ function loadJSON() {
 
                     <div class="product-content">
                         <h3 class="product-name">${product.name}</h3>
-                        <span class="product-category">
-                        ${product.category}
-                        </span>
+                        <span class="product-category">${product.category}</span>           
                         <p class="product-price">$${product.price}</p>
                     </div>
                 </div> `
@@ -59,4 +68,88 @@ function loadJSON() {
         .catch(err => {
             alert('Use live server or local server')
         })
+}
+
+// purchase product
+function purchaseProduct(e) {
+    if (e.target.classList.contains('add-to-cart-btn')) {
+        let product = e.target.parentElement.parentElement
+        getProductInfo(product);
+    }
+}
+
+// get product info
+function getProductInfo(product) {
+    let productInfo = {
+        id: cartItemID,
+        name: product.querySelector('.product-name').textContent,
+        category: product.querySelector('.product-category').textContent,
+        price: product.querySelector('.product-price').textContent,
+        imgSrc: product.querySelector('.product-img img').src
+    }
+    cartItemID++
+    addToCartList(productInfo);
+    saveProductInStorage(productInfo)
+}
+
+//add selected product to cart list
+function addToCartList(product) {
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.setAttribute(`data-id`, `${product.id}`);
+    cartItem.innerHTML = `
+            <img src="${product.imgSrc}" alt="product image">
+            <div class="cart-item-info">
+                <h3 class="cart-item-name">${product.name}</h3>
+                <span class="cart-item-category">${product.category}</span>
+                <span class="cart-item-price">${product.price}</span>
+            </div>
+            <button type="button" class="cart-item-del-btn">
+                <i class="fas fa-times"></i>
+            </button>
+        `
+    cartList.appendChild(cartItem)
+}
+
+// save the cart item to localstorage
+function saveProductInStorage(item) {
+    let products = getProductFromStorage();
+    products.push(item);
+    localStorage.setItem('products', JSON.stringify(products)
+    );
+}
+
+// get all the products if any form localstorage
+function getProductFromStorage() {
+    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : []
+    // returns empty array if nothing in localStorage
+}
+
+// load carts product
+function loadCart() {
+    let products = getProductFromStorage();
+    // update beginning cartID on refresh
+    if (products.length < 1) {
+        cartItemID = 1;
+    }
+    else {
+        cartItemID = products[products.length - 1].id;
+        cartItemID++;
+    }
+    products.forEach(product => addToCartList(product))
+}
+
+// calculate cart info
+function findCartInfo() {
+    let products = getProductFromStorage();
+    let total = products.reduce((acc, product) => {
+        let price = parseFloat(product.price.substr(1));
+        // remove the dollar sign
+        return acc += price
+    }, 0) //adding all the prices
+
+    return {
+        total: total.toFixed(2),
+        productCount: products.length
+    }
 }
